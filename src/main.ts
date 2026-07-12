9365f18d5935f02c3caafed21e332f8e4b967778
lowkanta <ricardosiqueirarf@gmail.com>
2026-07-12 07:32:31 -0300

    Reset repository for new project

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-import './business/holding.css';
-import { BusinessIdleApp } from './business/BusinessIdleApp';
-
-const root = document.getElementById('game');
-
-if (root) {
-  new BusinessIdleApp(root);
-}
